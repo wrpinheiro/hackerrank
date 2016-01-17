76cfb3e46ca6031e9d14b0f087a11a42fdc088bf
Wellington Pinheiro <Wellington.Pinheiro@wal-mart.com>
2016-01-16 22:47:12 -0200

    Solve hello world n times

--- src/hackerrank/introduction/hello-world-n-times.clj
+++ src/hackerrank/introduction/hello-world-n-times.clj
@@ -1,6 +1,8 @@
 (ns hackerrank.introduction.hello-world-n-times)
 
-(fn [n]
-  (loop [n])
-    (str "Hello World")
-    (recur (dec n)))
+(fn[n]
+    (loop [iteration n]
+        (if (> iteration 0)
+            (do
+                (println "Hello World")
+                (recur (dec iteration))))))
